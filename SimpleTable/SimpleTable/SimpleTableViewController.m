548f33f3c32b1d50ab0beca8a586c43ed50ce0ea
Marcus Eagan <marcuseagan@Marcuss-MacBook-Pro.local>
2015-09-20 02:34:27 -0700

    better foundation for table app

---
+++ SimpleTable/SimpleTable/SimpleTableViewController.m
@@ -0,0 +1,96 @@
+//
+//  SimpleTableViewController.m
+//  SimpleTable
+//
+//  Created by Simon Ng on 16/4/12.
+//  Copyright (c) 2012 AppCoda. All rights reserved.
+//
+
+#import "SimpleTableViewController.h"
+#import "SimpleTableCell.h"
+
+@interface SimpleTableViewController ()
+
+@end
+
+@implementation SimpleTableViewController
+{
+    NSMutableArray *tableData;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Initialize table data
+    tableData = [NSMutableArray arrayWithObjects:@"Egg Benedict", @"Mushroom Risotto", @"Full Breakfast", @"Hamburger", @"Ham and Egg Sandwich", @"Creme Brelee", @"White Chocolate Donut", @"Starbucks Coffee", @"Vegetable Curry", @"Instant Noodle with Egg", @"Noodle with BBQ Pork", @"Japanese Noodle with Pork", @"Green Tea", @"Thai Shrimp Cake", @"Angry Birds Cake", @"Ham and Cheese Panini", nil];
+    
+    NSString *baseURLStr = @"http://10.0.0.18:8732/inventory";
+    NSURLRequest *request = [NSURLRequest requestWithURL:[NSURL
+                                                          URLWithString:baseURLStr]];
+    NSURLResponse *response = nil;
+    NSError *error = nil;
+    NSData *data = [NSURLConnection sendSynchronousRequest:request
+                                         returningResponse:&response
+                                                     error:&error];
+    if (!data) {
+        NSLog(@"%s: sendSynchronousRequest error: %@", __FUNCTION__, error);
+        return;
+    } else if ([response isKindOfClass:[NSHTTPURLResponse class]]) {
+        NSInteger statusCode = [(NSHTTPURLResponse *)response statusCode];
+        if (statusCode != 200) {
+            NSLog(@"%s: sendSynchronousRequest status code != 200: response = %@", __FUNCTION__, response);
+            return;
+        }
+    }
+    
+    NSError *parseError = nil;
+    NSDictionary *dictionary = [NSJSONSerialization JSONObjectWithData:data options:0 error:&parseError];
+    if (!dictionary) {
+        NSLog(@"%s: JSONObjectWithData error: %@; data = %@", __FUNCTION__, parseError, [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding]);
+    }
+    
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return (interfaceOrientation != UIInterfaceOrientationPortraitUpsideDown);
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return [tableData count];
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    static NSString *simpleTableIdentifier = @"SimpleTableItem";
+    
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:simpleTableIdentifier];
+    
+    if (cell == nil) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:simpleTableIdentifier];
+    }
+    
+    cell.textLabel.text = [tableData objectAtIndex:indexPath.row];
+    cell.imageView.image = [UIImage imageNamed:@"creme_brelee.jpg"];
+
+    return cell;
+}
+
+- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    // Remove the row from data model
+    [tableData removeObjectAtIndex:indexPath.row];
+    
+    // Request table view to reload
+    [tableView reloadData];
+}
+
+@end
+
